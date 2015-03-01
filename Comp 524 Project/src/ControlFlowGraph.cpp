63c3f25d442d5656befe3da704e1292b187a7d9e
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-01 13:37:50 -0500

    I added a couple of simple functions to simulate the control flow graph similiar to how el ariss did it in ObjectiveF.java. We may end up changing how this is done later but for now we will at least be able to start working on other things.

--- Comp 524 Project/src/ControlFlowGraph.cpp
+++ Comp 524 Project/src/ControlFlowGraph.cpp
@@ -18,4 +18,38 @@ ControlFlowGraph::~ControlFlowGraph(){
 
 }
 
+bool * ControlFlowGraph::block1(TestCase testCase) {
+	int parameters = testCase.getInputParameters();
+	bool* coverage;
 
+	if(parameters[0] > 2)
+		coverage = block3(testCase);
+	else
+		coverage = block2(testCase);
+
+
+	coverage[0] = true;
+
+	return coverage;
+}
+
+bool * ControlFlowGraph::block2(TestCase testCase) {
+	bool coverage = block4(testCase);
+	coverage[1] = true;
+
+	return coverage;
+
+}
+
+bool * ControlFlowGraph::block3(TestCase testCase) {
+	bool coverage = block4(testCase);
+	coverage[2] = true;
+
+	return coverage;
+}
+
+bool * ControlFlowGraph::block4(TestCase testCase) {
+	bool coverage = {false,false, false, false};
+	coverage[3] = true;
+	return coverage;
+}
