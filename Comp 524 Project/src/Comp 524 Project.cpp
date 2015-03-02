7fcefa1cbdcad72a229ba7505aa8386d4ff35a52
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-02 00:24:42 -0500

    misc

--- Comp 524 Project/src/Comp 524 Project.cpp
+++ Comp 524 Project/src/Comp 524 Project.cpp
@@ -1,9 +1,19 @@
 
 
 #include <iostream>
+#include "TestCase.h"
+#include "TestSuite.h"
+#include "ControlFlowGraph.h"
 using namespace std;
 
 int main() {
-	cout << "Run" << endl;
+
+	TestCase* tc = new TestCase(4, 1);
+	int p[1];
+	p[0] = 3;
+	tc->setInputParameters(p);
+
+
+
 	return 0;
 }
