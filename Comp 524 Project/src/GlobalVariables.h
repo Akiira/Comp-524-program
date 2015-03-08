bfb5db0b20962883ec1fcff80726a529c25062b1
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-08 14:03:36 -0400

    Created a header file with the global variable targetCFG. This way we do not need to store it in any classes or pass it into functions.

---
+++ Comp 524 Project/src/GlobalVariables.h
@@ -0,0 +1,17 @@
+/*
+ * GlobalVariables.h
+ *
+ *  Created on: Mar 8, 2015
+ *      Author: Kitty
+ */
+
+#ifndef GLOBALVARIABLES_H_
+#define GLOBALVARIABLES_H_
+
+#include "ControlFlowGraph.h"
+
+extern ControlFlowGraph* targetCFG;
+
+
+
+#endif /* GLOBALVARIABLES_H_ */
