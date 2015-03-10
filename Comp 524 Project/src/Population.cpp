a2076b7dd98bc7b2878041f19347d3fc7faa5999
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-09 20:27:19 -0400

    added crossover for test cases

--- Comp 524 Project/src/Population.cpp
+++ Comp 524 Project/src/Population.cpp
@@ -100,7 +100,49 @@ void Population::crossover(const Organism& parent1, const Organism& parent2,
 	child2 = new Organism { parent2.getNumberOfTestCases(),	child2TestCases };
 }
 
+void Population::crossover(const TestCase& parent1, const TestCase& parent2,
+		TestCase*& child1, TestCase*& child2, int numberOfCutPoints) {
+	auto cutPoints = selectCutPoints(numberOfCutPoints, parent1.getNumberOfParameters());
+	bool alternate { true };
+	int current { 0 };
+
+	for (int i = 0; i < numberOfCutPoints; i++) {
+		if (alternate) {
+			for (int j = current; j <= cutPoints[i]; j++) {
+				child1->setInputParameterAtIndex(j, parent1.getInputParameterAtIndex(j));
+				child2->setInputParameterAtIndex(j, parent2.getInputParameterAtIndex(j));
+			}
+		}  //if
+		else {
+			for (int j = current; j <= cutPoints[i]; j++) {
+				child1->setInputParameterAtIndex(j, parent2.getInputParameterAtIndex(j));
+				child2->setInputParameterAtIndex(j, parent1.getInputParameterAtIndex(j));
+			}
+		}
+		current = cutPoints[i] + 1;
+		alternate = !alternate;
+	}
+
+	delete[] cutPoints;
+
+	if (alternate) {
+		for (int j = current; j < parent1.getNumberOfParameters(); j++) {
+			child1->setInputParameterAtIndex(j, parent1.getInputParameterAtIndex(j));
+			child2->setInputParameterAtIndex(j, parent2.getInputParameterAtIndex(j));
+		}
+	} else {
+		for (int j = current; j < parent1.getNumberOfParameters(); j++) {
+			child1->setInputParameterAtIndex(j, parent2.getInputParameterAtIndex(j));
+			child2->setInputParameterAtIndex(j, parent1.getInputParameterAtIndex(j));
+		}
+	}
+}
+
+
+
 int* Population::selectCutPoints(int numCutPoints, int upperBound) {
+	assert(numCutPoints < upperBound);
+
 	//select numCutPoints randomly in the range [0..orgLength-1]
 	//and store their locations in the cutPoints array
 	int* cutPoints = new int[numCutPoints];
