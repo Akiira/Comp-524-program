29a8a1b8cedc1e5800233ccc690aed6bbdf5ad7a
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-11 22:45:47 -0400

    added static int to denote what type of scaling is being used.

--- Comp 524 Project/src/Population.h
+++ Comp 524 Project/src/Population.h
@@ -40,5 +40,8 @@ private:
 	void setPopulationFitness();
 	int* selectCutPoints(int numberOfCutPoints, int upperBound);
 
+	//TODO remove hard coding of value
+	const static typeOfScaling SCALING = NONE;
+
 };
 #endif // !defined(EA_BE7C8399_4BC5_48f6_93FA_2B1C440AB247__INCLUDED_)
