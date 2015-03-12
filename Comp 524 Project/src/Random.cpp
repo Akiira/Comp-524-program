5bf23081e55a2d7c4dd00f02fa97649c596ab96c
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-11 21:53:11 -0400

    temp

--- Comp 524 Project/src/Random.cpp
+++ Comp 524 Project/src/Random.cpp
@@ -8,8 +8,21 @@
 
 #include "Random.h"
 
-random_device seed;
-mt19937 mersenne(seed());  //Mersenne Twister random number generator
+//random_device seed;
+//mt19937 mersenne(seed());  //Mersenne Twister random number generator
+//long uniformInRange(long from, long to)
+//{// generate a random uniformly in the range [from, to]
+//  uniform_int_distribution<long> uniformFT(from, to);
+//  return uniformFT(mersenne);
+//}
+//
+//
+////uniform01() generates a random double uniformly in the range [0,1)
+//function<double()> uniform01 = bind(uniform_real_distribution<double>(0,1), mt19937(seed()));
+
+#include <ctime>
+int seed = time(NULL);
+mt19937 mersenne(seed);  //Mersenne Twister random number generator
 long uniformInRange(long from, long to)
 {// generate a random uniformly in the range [from, to]
   uniform_int_distribution<long> uniformFT(from, to);
@@ -18,4 +31,4 @@ long uniformInRange(long from, long to)
 
 
 //uniform01() generates a random double uniformly in the range [0,1)
-function<double()> uniform01 = bind(uniform_real_distribution<double>(0,1), mt19937(seed()));
+function<double()> uniform01 = bind(uniform_real_distribution<double>(0,1), mt19937(seed));
