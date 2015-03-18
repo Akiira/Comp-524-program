e49915e833cfa31e2e17c1157a0e3f0aa7bb1a3d
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-18 10:36:19 -0400

    Linear scaling will never be negative now.

--- Comp 524 Project/src/Population.h
+++ Comp 524 Project/src/Population.h
@@ -43,7 +43,7 @@ private:
 	void computeEdgeCoverage();
 
 	//TODO remove hard coding of value
-	const static typeOfScaling SCALING = NONE;
+	const static typeOfScaling SCALING = LINEAR;
 
 };
 #endif // !defined(EA_BE7C8399_4BC5_48f6_93FA_2B1C440AB247__INCLUDED_)
