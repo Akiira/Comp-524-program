dff3749ca0573107207d93b9658e5bd1a5b4131a
Randall Hudson <Hudsonrj09@gmail.com>
2015-05-01 03:20:21 -0400

    removed all those unused booleans

--- Comp 524 Project/src/GlobalVariables.h
+++ Comp 524 Project/src/GlobalVariables.h
@@ -8,7 +8,6 @@
 #ifndef GLOBALVARIABLES_H_
 #define GLOBALVARIABLES_H_
 
-//#include "ControlFlowGraph.h"
 class RangeSet;
 class ControlFlowGraph;
 class Population;
@@ -16,22 +15,6 @@ class Population;
 extern ControlFlowGraph* targetCFG;
 extern RangeSet* rangeSet;
 extern Population* globalPopulation;
-
-extern bool printPopFitnessOnce;
-extern bool printPopFitness;
-extern bool printGenerationAndRatio;
-extern bool printCoverageRatioForTScrossover;
-extern bool printCoverageRatioForTScrossoverOnce;
-extern bool printCoverageRatioForTCcrossover;
-extern bool printCoverageRatioForTCcrossoverOnce;
-extern bool printReplacement;
-
-extern bool changeLocalOpt;
-
-extern bool pauseOnTSCrossover;
-extern bool pauseOnTCcrossover;
-extern bool pauseONlocalOpt;
-extern bool pause;
 
 typedef int typeOfScaling;
 enum {NONE, LINEAR, EXPONENTIAL, RANKED, SHARING};
