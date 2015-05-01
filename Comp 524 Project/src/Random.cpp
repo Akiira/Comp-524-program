19ded6d6d60bd692f42079aeb9953550d8e81a03
Randall Hudson <Hudsonrj09@gmail.com>
2015-05-01 03:01:47 -0400

    removed commented out code. Also, did that normal dist function come from somewhere? it looks like maybe it did, so we should cite it if u remember where u found it.

--- Comp 524 Project/src/Random.cpp
+++ Comp 524 Project/src/Random.cpp
@@ -8,20 +8,9 @@
 
 #include "Random.h"
 
-//random_device seed;
-//mt19937 mersenne(seed());  //Mersenne Twister random number generator
-//long uniformInRange(long from, long to)
-//{// generate a random uniformly in the range [from, to]
-//  uniform_int_distribution<long> uniformFT(from, to);
-//  return uniformFT(mersenne);
-//}
-//
-//
-////uniform01() generates a random double uniformly in the range [0,1)
-//function<double()> uniform01 = bind(uniform_real_distribution<double>(0,1), mt19937(seed()));
-
 #include <ctime>
 #include <cassert>
+
 int seed = time(NULL);
 mt19937 mersenne(seed);  //Mersenne Twister random number generator
 long uniformInRange(long from, long to)
