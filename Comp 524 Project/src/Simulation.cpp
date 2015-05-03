20412ca0f47ac8a8ec397fcb73162c792602e822
amb6470 <amb6470@ubuntu>
2015-05-02 23:21:41 -0400

    small

--- Comp 524 Project/src/Simulation.cpp
+++ Comp 524 Project/src/Simulation.cpp
@@ -68,7 +68,7 @@ int Simulation::run(int numberOfGenerations, int numberOfCutPoints, double mutat
 			gensOfNoImprov = 0;
 		}
 
-		if (gensOfNoImprov >= 30 && currentGen % 100 == 1) {
+		if (gensOfNoImprov >= 20 && currentGen % 100 == 1) {
 			rangeSet->adaptRangesBasedOnUsefulness();
 		}
 
